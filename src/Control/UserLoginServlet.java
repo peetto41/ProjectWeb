f997b6308acf3c56a7f5f557b9b266aebc6f7874
win10 <peet>
2020-03-29 20:57:48 +0700

    Edit Line 17

--- src/Control/UserLoginServlet.java
+++ src/Control/UserLoginServlet.java
@@ -14,6 +14,6 @@ public class UserLoginServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("Test");
+        System.out.println("Testtะ");
     }
 }
